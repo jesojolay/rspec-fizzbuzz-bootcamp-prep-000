52579c2b70214af7eaa8a567c94a149b78dec267
jasmine Magdi <jesojolay@yahoo.com>
2017-09-14 21:39:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(n)
     if n % 3 == 0 && n % 5 == 0
-         "FizzBuzz"
+         puts "FizzBuzz"
     elsif n % 3 == 0
-         "Fizz"
+        puts  "Fizz"
     elsif n % 5 ==0
-         "Buzz"
+        puts "Buzz"
     else
-         nil
+        puts nil
     end
 end
